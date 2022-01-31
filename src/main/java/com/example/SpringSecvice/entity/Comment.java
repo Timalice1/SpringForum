5762ac394f6233e1d18187d55f24237f424a5bf4
Timalice1 <newacccc2@gmail.com>
2022-01-31 11:59:38 +0200

    Update styles and delombooks entity's

--- src/main/java/com/example/SpringSecvice/entity/Comment.java
+++ src/main/java/com/example/SpringSecvice/entity/Comment.java
@@ -1,12 +1,9 @@
 package com.example.SpringSecvice.entity;
 
-import lombok.Data;
-
 import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@Data
 public class Comment {
 
     @Id
@@ -19,4 +16,82 @@ public class Comment {
     private String text;
 
     private Date date;
+
+    public Comment() {
+    }
+
+    public Integer getComment_id() {
+        return this.comment_id;
+    }
+
+    public Post getPost() {
+        return this.post;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    public Date getDate() {
+        return this.date;
+    }
+
+    public void setComment_id(Integer comment_id) {
+        this.comment_id = comment_id;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof Comment)) return false;
+        final Comment other = (Comment) o;
+        if (!other.canEqual((Object) this)) return false;
+        final Object this$comment_id = this.getComment_id();
+        final Object other$comment_id = other.getComment_id();
+        if (this$comment_id == null ? other$comment_id != null : !this$comment_id.equals(other$comment_id))
+            return false;
+        final Object this$post = this.getPost();
+        final Object other$post = other.getPost();
+        if (this$post == null ? other$post != null : !this$post.equals(other$post)) return false;
+        final Object this$text = this.getText();
+        final Object other$text = other.getText();
+        if (this$text == null ? other$text != null : !this$text.equals(other$text)) return false;
+        final Object this$date = this.getDate();
+        final Object other$date = other.getDate();
+        if (this$date == null ? other$date != null : !this$date.equals(other$date)) return false;
+        return true;
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof Comment;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $comment_id = this.getComment_id();
+        result = result * PRIME + ($comment_id == null ? 43 : $comment_id.hashCode());
+        final Object $post = this.getPost();
+        result = result * PRIME + ($post == null ? 43 : $post.hashCode());
+        final Object $text = this.getText();
+        result = result * PRIME + ($text == null ? 43 : $text.hashCode());
+        final Object $date = this.getDate();
+        result = result * PRIME + ($date == null ? 43 : $date.hashCode());
+        return result;
+    }
+
+    public String toString() {
+        return "Comment(comment_id=" + this.getComment_id() + ", post=" + this.getPost() + ", text=" + this.getText() + ", date=" + this.getDate() + ")";
+    }
 }
--- src/main/java/com/example/SpringSecvice/entity/Post.java
+++ src/main/java/com/example/SpringSecvice/entity/Post.java
@@ -1,17 +1,12 @@
 package com.example.SpringSecvice.entity;
 
 
-import lombok.Data;
-
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 import java.util.Date;
 import java.util.List;
 
-import com.example.SpringSecvice.entity.Comment;
-
-@Data
 @Entity
 @Table(name = "post")
 public class Post {
@@ -38,6 +33,9 @@ public class Post {
 
     private int views;
 
+    public Post() {
+    }
+
     public int commentCount(){
         return comments.size();
     }
@@ -54,4 +52,128 @@ public class Post {
     }
 
 
+    public Long getPost_id() {
+        return this.post_id;
+    }
+
+    public @NotEmpty(message = "This field cannot be null") @Size(min = 5, message = "At least 5 chars") String getTitle() {
+        return this.title;
+    }
+
+    public @NotEmpty(message = "This field cannot be null") String getDescription() {
+        return this.description;
+    }
+
+    public Date getDate() {
+        return this.date;
+    }
+
+    public List<Comment> getComments() {
+        return this.comments;
+    }
+
+    public String getCreationDate() {
+        return this.creationDate;
+    }
+
+    public String getPhotoName() {
+        return this.photoName;
+    }
+
+    public int getViews() {
+        return this.views;
+    }
+
+    public void setPost_id(Long post_id) {
+        this.post_id = post_id;
+    }
+
+    public void setTitle(@NotEmpty(message = "This field cannot be null") @Size(min = 5, message = "At least 5 chars") String title) {
+        this.title = title;
+    }
+
+    public void setDescription(@NotEmpty(message = "This field cannot be null") String description) {
+        this.description = description;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+
+    public void setCreationDate(String creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public void setPhotoName(String photoName) {
+        this.photoName = photoName;
+    }
+
+    public void setViews(int views) {
+        this.views = views;
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof Post)) return false;
+        final Post other = (Post) o;
+        if (!other.canEqual((Object) this)) return false;
+        final Object this$post_id = this.getPost_id();
+        final Object other$post_id = other.getPost_id();
+        if (this$post_id == null ? other$post_id != null : !this$post_id.equals(other$post_id)) return false;
+        final Object this$title = this.getTitle();
+        final Object other$title = other.getTitle();
+        if (this$title == null ? other$title != null : !this$title.equals(other$title)) return false;
+        final Object this$description = this.getDescription();
+        final Object other$description = other.getDescription();
+        if (this$description == null ? other$description != null : !this$description.equals(other$description))
+            return false;
+        final Object this$date = this.getDate();
+        final Object other$date = other.getDate();
+        if (this$date == null ? other$date != null : !this$date.equals(other$date)) return false;
+        final Object this$comments = this.getComments();
+        final Object other$comments = other.getComments();
+        if (this$comments == null ? other$comments != null : !this$comments.equals(other$comments)) return false;
+        final Object this$creationDate = this.getCreationDate();
+        final Object other$creationDate = other.getCreationDate();
+        if (this$creationDate == null ? other$creationDate != null : !this$creationDate.equals(other$creationDate))
+            return false;
+        final Object this$photoName = this.getPhotoName();
+        final Object other$photoName = other.getPhotoName();
+        if (this$photoName == null ? other$photoName != null : !this$photoName.equals(other$photoName)) return false;
+        if (this.getViews() != other.getViews()) return false;
+        return true;
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof Post;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $post_id = this.getPost_id();
+        result = result * PRIME + ($post_id == null ? 43 : $post_id.hashCode());
+        final Object $title = this.getTitle();
+        result = result * PRIME + ($title == null ? 43 : $title.hashCode());
+        final Object $description = this.getDescription();
+        result = result * PRIME + ($description == null ? 43 : $description.hashCode());
+        final Object $date = this.getDate();
+        result = result * PRIME + ($date == null ? 43 : $date.hashCode());
+        final Object $comments = this.getComments();
+        result = result * PRIME + ($comments == null ? 43 : $comments.hashCode());
+        final Object $creationDate = this.getCreationDate();
+        result = result * PRIME + ($creationDate == null ? 43 : $creationDate.hashCode());
+        final Object $photoName = this.getPhotoName();
+        result = result * PRIME + ($photoName == null ? 43 : $photoName.hashCode());
+        result = result * PRIME + this.getViews();
+        return result;
+    }
+
+    public String toString() {
+        return "Post(post_id=" + this.getPost_id() + ", title=" + this.getTitle() + ", description=" + this.getDescription() + ", date=" + this.getDate() + ", comments=" + this.getComments() + ", creationDate=" + this.getCreationDate() + ", photoName=" + this.getPhotoName() + ", views=" + this.getViews() + ")";
+    }
 }
